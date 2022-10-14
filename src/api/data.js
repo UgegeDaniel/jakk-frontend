fe9ae5a8c4e2740f639d1092bc0a23f8d6a11c60
Ugege Daniel <36529717+UgegeDaniel@users.noreply.github.com>
2022-10-15 00:30:43 +0100

    Update data.js

--- src/api/data.js
+++ src/api/data.js
@@ -10,6 +10,7 @@ const baseUrl = 'https://questions.aloc.com.ng/api/'
 const questionsUrl = `${baseUrl}/v2/q?`
 const subjectsUrl = `${baseUrl}metrics/list-subjects`
 const yearsUrl = '${baseUrl}metrics/years-available-for'
+
 export const fetchSubjects = async () => {
     try {
         const response = await (subjectsUrl, options)
