ee101b2fd2204a1ecfd6cff94ee0452d8da4b0fc
Ugege Daniel <36529717+UgegeDaniel@users.noreply.github.com>
2022-10-15 18:36:51 +0100

    Update handleTestStart.js

--- src/utils/handleTestStart.js
+++ src/utils/handleTestStart.js
@@ -3,10 +3,10 @@ export const handleTestStart = async (values, functions) => {
     const {subject, year, testParams} = values 
     setTestParams({ subject: '', year: '', examtype: 'utme' })
     if (!subject) {
-        setNotification({ show: true, msg: 'Please pick a subject', type: "danger" })
+        //setNotification({ show: true, msg: 'Please pick a subject', type: "danger" })
         return
     } else if (!year) {
-        setNotification({ show: true, msg: 'Please pick a year', type: "danger" })
+        //setNotification({ show: true, msg: 'Please pick a year', type: "danger" })
         return
     } else {
         setTimer({ hour: 2, minute: 0, second: 0 })
