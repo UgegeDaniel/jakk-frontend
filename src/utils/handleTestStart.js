f2986d2914781a6afda8705bed8012605a13d7e6
Ugege Daniel <36529717+UgegeDaniel@users.noreply.github.com>
2022-10-15 20:31:35 +0100

    Update handleTestStart.js

--- src/utils/handleTestStart.js
+++ src/utils/handleTestStart.js
@@ -1,15 +1,12 @@
 export const handleTestStart = async (values, functions) => {
     const {navigate, testStart} = functions
     const {subject, year, testParams} = values 
-    
     if (!subject) {
-        //setNotification({ show: true, msg: 'Please pick a subject', type: "danger" })
         return
     } else if (!year) {
-        //setNotification({ show: true, msg: 'Please pick a year', type: "danger" })
         return
     } else {
-            testStart()
-            navigate('/questions')
+        await testStart()
+        navigate('/questions')
     }
 }
