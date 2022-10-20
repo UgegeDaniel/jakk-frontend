5f96ae7f3a0178b0a41fa08636132f13220207ec
Ugege Daniel <36529717+UgegeDaniel@users.noreply.github.com>
2022-10-20 14:55:22 +0100

    Update Param.jsx

--- src/components/Params/Param.jsx
+++ src/components/Params/Param.jsx
@@ -1,17 +1,15 @@
 import { Button } from '@material-ui/core'
 import {useState} from 'react'
 const Param = ({ items, testParams, setTestParams, className, feild }) => {
-    const [clicked, setClicked ] = useState(false)
+    //const [clicked, setClicked ] = useState(false)
 const onClickHandler = (e) => {
         setTestParams({ ...testParams, [feild]: e.target.textContent })
-        if(e.target.textContent === e.target.value){
-          setClicked(true)
-         }
+        
     }
     return (
         <ul>
             {items?.length !== 0 && items?.map((item, index) =>
-                <Button key={index} value={item} className={className} onClick={onClickHandler} variant= {clicked ? 'contained' : 'outlined'} color={index % 2 === 1 ? "primary" : "secondary"} size="small">
+                <Button key={index} value={item} className={className} onClick={onClickHandler} variant= {index ? 'contained' : 'outlined'} color={index % 2 === 1 ? "primary" : "secondary"} size="small">
                     {item}
                 </Button>
             )}
